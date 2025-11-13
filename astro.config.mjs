35afbeb513e6c0ffd884ead65e562f32e99c1d32
kenta377-hue <kenta.377@icloud.com>
2025-11-13 07:07:35 +0000

    11/13/16:07

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  adapter: cloudflare(),
+  output: "static",
 });
